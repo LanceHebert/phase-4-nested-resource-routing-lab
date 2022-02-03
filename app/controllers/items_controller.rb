caaac4c94c3f7637084e8bfeb4b45b4c6f34bc6e
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-03 10:29:03 -0800

    commit tree a86c40781721ac97c53f72930436407267b8f44c

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show   
+    items = Item.find(params[:id]) 
+    render json: items, include: :user
+  end
+
+  def create
+   
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+    
+    
+    render json: item, status: :created
+  end
+
+
+
+private
+
+  def render_not_found_response
+    render json: {error: "Record not found"}, status: :not_found
+
+  end
+
+  def item_params
+    params.permit(:description,:name,:price)
   end
 
 end
